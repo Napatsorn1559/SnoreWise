376c5254dfa0d8725b08fdb0b4387f8e5f4bb201
Kanyaphach <6322773068@g.siit.tu.ac.th>
2023-11-30 00:16:57 +0700

    update Requestlog

--- screen/RequestLog.js
+++ screen/RequestLog.js
@@ -1,29 +1,61 @@
-import React , { useState} from "react";
-import { View, Text, StyleSheet, ImageBackground,TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ImageBackground, TextInput, Button, TouchableWithoutFeedback } from "react-native";
 import waveBG from '../assets/top-bottom-bg.png';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
-
-export default function RequestLog(){
+export default function RequestLog() {
     const [date, setDate] = useState(new Date());
-    const [showPicker, setShowPicker] = useState(false);
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
-    return(
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+    };
+
+    const handleConfirm = (selectedDate) => {
+        hideDatePicker();
+        if (selectedDate) {
+            setDate(selectedDate);
+        }
+    };
+
+    return (
         <View style={{ flex: 1 }}>
-        <ImageBackground style={styles.container} source={waveBG} >
-            <Text style={styles.tabHeaderText}>Request <Text style={{color: 'yellow'}}>Log</Text></Text>
-            <TextInput style={styles.inputBox} placeholder="File type" placeholderTextColor="#fff"></TextInput>
-            <TextInput style={styles.inputBox} placeholder="Start Date" placeholderTextColor="#fff"></TextInput>
-            <TextInput style={styles.inputBox} placeholder="End Date" placeholderTextColor="#fff"></TextInput>
-            <TextInput style={styles.inputBox} placeholder="Purpose" placeholderTextColor="#fff"></TextInput>
-            { showPicker && (
-                <DateTimePicker mode='date' display='spinner' value={date} />
-            )}
-            <Button title='Submit' color='#03A9F4' onPress={()=>{alert('Submitted')}} />
-        </ImageBackground>
-    </View>
-
-    )
+            <ImageBackground style={styles.container} source={waveBG} >
+                <Text style={styles.tabHeaderText}>Request <Text style={{ color: 'yellow' }}>Log</Text></Text>
+                <TextInput style={styles.inputBox} placeholder="File type" placeholderTextColor="#fff" />
+                <Text style={styles.Inputtext}>Start Date</Text>
+                <TouchableWithoutFeedback onPress={showDatePicker}>
+                    <View style={styles.datePickerStyle}>
+                        <Text style={styles.dateText}>{date.toLocaleDateString()}</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+                <DateTimePickerModal
+                    isVisible={isDatePickerVisible}
+                    mode="date"
+                    onConfirm={handleConfirm}
+                    onCancel={hideDatePicker}
+                />
+                <Text style={styles.Inputtext}>End Date</Text>
+                <TouchableWithoutFeedback onPress={showDatePicker}>
+                    <View style={styles.datePickerStyle}>
+                        <Text style={styles.dateText}>{date.toLocaleDateString()}</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+                <DateTimePickerModal
+                    isVisible={isDatePickerVisible}
+                    mode="date"
+                    onConfirm={handleConfirm}
+                    onCancel={hideDatePicker}
+                />
+                <TextInput style={styles.inputBox} placeholder="Purpose" placeholderTextColor="#fff" />
+                <Button title='Submit' color='#03A9F4' onPress={() => { alert('Submitted') }} />
+            </ImageBackground>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -37,18 +69,38 @@ const styles = StyleSheet.create({
         fontSize: 45,
         color: 'white',
         fontWeight: 'bold',
-        // flex:1,
         textAlign: 'center',
         marginBottom: 20
     },
-    inputBox : {
-        backgroundColor:'rgba(255, 255, 255, 0.3)', 
-        height: 50, 
+    datePickerStyle: {
+        backgroundColor: 'rgba(255, 255, 255, 0.3)',
+        height: 50,
+        marginHorizontal: 20,
+        borderBottomColor: 'rgba(255, 229, 0, 1)',
+        borderBottomWidth: 2,
+        justifyContent: 'center',
+        paddingLeft: 10,
+        fontSize: 18,
+        marginBottom: 15
+    },
+    dateText: {
+        fontSize: 18,
+        color: 'white',
+    },
+    inputBox: {
+        backgroundColor: 'rgba(255, 255, 255, 0.3)',
+        height: 50,
         marginHorizontal: 20,
         borderBottomColor: 'rgba(255, 229, 0, 1)',
         borderBottomWidth: 2,
         paddingLeft: 10,
         fontSize: 18,
         marginBottom: 15
-    }
+    },
+    Inputtext: {
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold',
+        marginHorizontal: 20,
+    },
 });
